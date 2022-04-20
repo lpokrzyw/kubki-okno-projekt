19f6717a6f41aa0e60db02156250e9bc5462ef5e
LukpoDevs <71985109+LukpoDevs@users.noreply.github.com>
2022-04-20 19:00:15 +0200

    sharing Spring initial files

---
+++ kubki/src/main/java/com/okno/kubki/KubkiApplication.java
@@ -0,0 +1,13 @@
+package com.okno.kubki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubkiApplication.class, args);
+	}
+
+}
